caf152cf1788a9a75fead2db6e8c4f5ff2fb2076
bryanbilek <you@example.com>
2020-07-28 11:41:19 -0500

    added generations counter for grid evolutions

--- conways-game-of-life/src/components/Grid.js
+++ conways-game-of-life/src/components/Grid.js
@@ -12,8 +12,8 @@ const neighborOperations = [
   [1, 1],
   [-1, -1],
   [1, 0],
-  [-1, 0]
-]
+  [-1, 0],
+];
 
 const emptyGrid = () => {
   const rows = [];
@@ -21,12 +21,12 @@ const emptyGrid = () => {
     rows.push(Array.from(Array(gridCols), () => 0));
   }
   return rows;
-}
+};
 
 function Grid() {
   //grid creation
   const [grid, setGrid] = useState(() => {
-    return emptyGrid();   
+    return emptyGrid();
   });
 
   //setting state for each evolution
@@ -34,92 +34,106 @@ function Grid() {
 
   const handleStartStop = () => {
     setEvolutions(!evolutions);
-    if(!evolutions){
+    if (!evolutions) {
       evolutionsRef.current = true;
       runEvolutions();
     }
   };
 
+  const [generations, setGenerations] = useState(0);
+
   const evolutionsRef = useRef();
-  evolutionsRef.current = evolutions
+  evolutionsRef.current = evolutions;
 
   const runEvolutions = useCallback(() => {
-    if(!evolutionsRef.current) {
+    if (!evolutionsRef.current) {
       return;
     }
 
-    setGrid(grid => {
-      return produce(grid, gridcopy => {
-        for(let i = 0; i < gridRows; i++){
-          for(let k = 0; k < gridCols; k++){
+    setGrid((grid) => {
+      return produce(grid, (gridcopy) => {
+        for (let i = 0; i < gridRows; i++) {
+          for (let k = 0; k < gridCols; k++) {
             let neighborCells = 0;
 
             neighborOperations.forEach(([x, y]) => {
               const newI = i + x;
               const newK = k + y;
 
-              if(newI >= 0 && newI < gridRows && newK >= 0 && newK < gridCols){
-                neighborCells += grid[newI][newK]
+              if (
+                newI >= 0 &&
+                newI < gridRows &&
+                newK >= 0 &&
+                newK < gridCols
+              ) {
+                neighborCells += grid[newI][newK];
               }
-            })
+            });
 
-            if(neighborCells < 2 || neighborCells > 3){
+            if (neighborCells < 2 || neighborCells > 3) {
               gridcopy[i][k] = 0;
-            } else if(grid[i][k] === 0 && neighborCells === 3){
+            } else if (grid[i][k] === 0 && neighborCells === 3) {
               gridcopy[i][k] = 1;
             }
           }
         }
-      })
-    })
+      });
+    });
+    setGenerations(g => g + 1)
     setTimeout(runEvolutions, 1000);
-  }, [])
+  }, []);
 
   const handleClear = () => {
     setGrid(emptyGrid());
-  }
+    setGenerations(0)
+  };
 
   const handleRandom = () => {
     const rows = [];
     for (let i = 0; i < gridRows; i++) {
-      rows.push(Array.from(Array(gridCols), () => Math.random() > 0.7 ? 1 : 0));
+      rows.push(
+        Array.from(Array(gridCols), () => (Math.random() > 0.7 ? 1 : 0))
+      );
     }
-    setGrid(rows);    
-  }
-
-  const [generations, setGenerations] = useState(0);
+    setGrid(rows);
+  };
 
   return (
     <>
-    <button onClick={handleStartStop}>{evolutions ? "Stop" : "Start"}</button>
-    <button onClick={handleClear}>Clear</button>
-    <button onClick={handleRandom}>Random</button>
-    <div
-      style={{
-        display: "grid",
-        gridTemplateColumns: `repeat(${gridCols}, 20px)`,
-      }}
-    >
-      {grid.map((rows, i) =>
-        rows.map((col, k) => (
-          <div
-            key={`{i}-{k}`}
-            onClick={() => {
-              const newGrid = produce(grid, (gridcopy) => {
-                gridcopy[i][k] = grid[i][k] ? 0 : 1;
-              });
-              setGrid(newGrid);
-            }}
-            style={{
-              width: 20,
-              height: 20,
-              backgroundColor: grid[i][k] ? "black" : undefined,
-              border: "1px solid red",
-            }}
-          />
-        ))
-      )}
-    </div>
+      <button onClick={handleStartStop}>{evolutions ? "Stop" : "Start"}</button>
+      <button onClick={handleClear}>Clear</button>
+      <button onClick={handleRandom}>Random</button>
+      <div>Generations: {generations}</div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${gridCols}, 20px)`,
+        }}
+      >
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <div
+              key={`{i}-{k}`}
+              onClick={() => {
+                if (!evolutions) {
+                  const newGrid = produce(grid, (gridcopy) => {
+                    gridcopy[i][k] = grid[i][k] ? 0 : 1;
+                  });
+                  setGrid(newGrid);
+                } else {
+                  return;
+                }
+              }}
+              style={{
+                width: 20,
+                height: 20,
+                backgroundColor: grid[i][k] ? "black" : undefined,
+                border: "1px solid red",
+              }}
+            />
+          ))
+        )}
+      </div>
     </>
   );
 }
