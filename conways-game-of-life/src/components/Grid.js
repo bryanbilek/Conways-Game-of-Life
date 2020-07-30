25b0ce23b77d12135ec98ada1899ef9f9fcf73e7
bryanbilek <bbilek35@gmail.com>
2020-07-29 22:04:38 -0500

    added grid presets for a plus sign & 'hello'

--- conways-game-of-life/src/components/Grid.js
+++ conways-game-of-life/src/components/Grid.js
@@ -79,13 +79,13 @@ function Grid() {
         }
       });
     });
-    setGenerations(g => g + 1)
+    setGenerations((g) => g + 1);
     setTimeout(runEvolutions, 1000);
   }, []);
 
   const handleClear = () => {
     setGrid(emptyGrid());
-    setGenerations(0)
+    setGenerations(0);
   };
 
   const handleRandom = () => {
@@ -98,16 +98,90 @@ function Grid() {
     setGrid(rows);
   };
 
+  const handlePresetPlus = () => {
+    if (!evolutions) {
+      const newGrid = produce(grid, (gridcopy) => {
+        gridcopy[12][13] = grid[12][13] ? 0 : 1;
+        gridcopy[12][12] = grid[12][12] ? 0 : 1;
+        gridcopy[12][11] = grid[12][11] ? 0 : 1;
+        gridcopy[13][12] = grid[13][12] ? 0 : 1;
+        gridcopy[11][12] = grid[11][12] ? 0 : 1;
+      });
+      setGrid(newGrid);
+    }
+  };
+
+  const handlePresetHello = () => {
+    if (!evolutions) {
+      const newGrid = produce(grid, (gridcopy) => {
+        gridcopy[10][3] = grid[10][3] ? 0 : 1;
+        gridcopy[11][3] = grid[11][3] ? 0 : 1;
+        gridcopy[12][3] = grid[12][3] ? 0 : 1;
+        gridcopy[13][3] = grid[13][3] ? 0 : 1;
+        gridcopy[14][3] = grid[14][3] ? 0 : 1;
+        gridcopy[12][4] = grid[12][4] ? 0 : 1;
+        gridcopy[10][5] = grid[10][5] ? 0 : 1;
+        gridcopy[11][5] = grid[11][5] ? 0 : 1;
+        gridcopy[12][5] = grid[12][5] ? 0 : 1;
+        gridcopy[13][5] = grid[13][5] ? 0 : 1;
+        gridcopy[14][5] = grid[14][5] ? 0 : 1;
+
+        gridcopy[10][7] = grid[10][7] ? 0 : 1;
+        gridcopy[11][7] = grid[11][7] ? 0 : 1;
+        gridcopy[12][7] = grid[12][7] ? 0 : 1;
+        gridcopy[13][7] = grid[13][7] ? 0 : 1;
+        gridcopy[14][7] = grid[14][7] ? 0 : 1;
+        gridcopy[10][8] = grid[10][8] ? 0 : 1;
+        gridcopy[12][8] = grid[12][8] ? 0 : 1;
+        gridcopy[14][8] = grid[14][8] ? 0 : 1;
+
+        gridcopy[10][10] = grid[10][10] ? 0 : 1;
+        gridcopy[11][10] = grid[11][10] ? 0 : 1;
+        gridcopy[12][10] = grid[12][10] ? 0 : 1;
+        gridcopy[13][10] = grid[13][10] ? 0 : 1;
+        gridcopy[14][10] = grid[14][10] ? 0 : 1;
+        gridcopy[14][11] = grid[14][11] ? 0 : 1;
+        gridcopy[14][12] = grid[14][12] ? 0 : 1;
+
+        gridcopy[10][14] = grid[10][14] ? 0 : 1;
+        gridcopy[11][14] = grid[11][14] ? 0 : 1;
+        gridcopy[12][14] = grid[12][14] ? 0 : 1;
+        gridcopy[13][14] = grid[13][14] ? 0 : 1;
+        gridcopy[14][14] = grid[14][14] ? 0 : 1;
+        gridcopy[14][15] = grid[14][15] ? 0 : 1;
+        gridcopy[14][16] = grid[14][16] ? 0 : 1;
+
+        gridcopy[10][18] = grid[10][18] ? 0 : 1;
+        gridcopy[11][18] = grid[11][18] ? 0 : 1;
+        gridcopy[12][18] = grid[12][18] ? 0 : 1;
+        gridcopy[13][18] = grid[13][18] ? 0 : 1;
+        gridcopy[14][18] = grid[14][18] ? 0 : 1;
+        gridcopy[10][20] = grid[10][20] ? 0 : 1;
+        gridcopy[11][20] = grid[11][20] ? 0 : 1;
+        gridcopy[12][20] = grid[12][20] ? 0 : 1;
+        gridcopy[13][20] = grid[13][20] ? 0 : 1;
+        gridcopy[14][20] = grid[14][20] ? 0 : 1;
+        gridcopy[10][19] = grid[10][19] ? 0 : 1;
+        gridcopy[14][19] = grid[14][19] ? 0 : 1;
+      });
+      setGrid(newGrid);
+    }
+  };
+
   return (
     <>
       <button onClick={handleStartStop}>{evolutions ? "Stop" : "Start"}</button>
       <button onClick={handleClear}>Clear</button>
       <button onClick={handleRandom}>Random</button>
+      <button onClick={handlePresetPlus}>Plus</button>
+      <button onClick={handlePresetHello}>'Hello'</button>
       <div>Generations: {generations}</div>
       <div
         style={{
           display: "grid",
+          textAlign: "center",
           gridTemplateColumns: `repeat(${gridCols}, 20px)`,
+          width: "100%",
         }}
       >
         {grid.map((rows, i) =>
@@ -128,7 +202,7 @@ function Grid() {
                 width: 20,
                 height: 20,
                 backgroundColor: grid[i][k] ? "black" : undefined,
-                border: "1px solid red",
+                border: "1px solid black",
               }}
             />
           ))
